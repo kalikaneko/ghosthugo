5dfb10e271d066e739730c532a91786fc0bcc2f9
kali <kalikaneko@riseup.net>
2024-01-13 21:07:48 +0100

    do not fail if HUGO_CONTENT is not defined

--- ghosthugo_entrypoint.sh
+++ ghosthugo_entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
+# Check if content dir is defined, fallback to a default if not
+CONTENT=${HUGO_CONTENT:-content/posts}
 cd /site
 echo "[+] npm install"
 npm install || echo "npm install failed"
 echo "[+] importing ghost content..."
 /usr/local/bin/ghosthugo
-ls -latr content/posts
+ls -latr ${CONTENT}
 echo "[+] running hugo"
 hugo "$@"
